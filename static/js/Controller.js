38a797dd613451248c5853d736ba40e8cba6992f
Erik Hazzard <enoex1@gmail.com>
2014-01-04 21:02:07 -0800

    updates mobile for ios

--- static/js/Controller.js
+++ static/js/Controller.js
@@ -39,25 +39,13 @@ define([
 
         setupMobile: function setupMobile(){
             // Mobile related functionality
-             $(window)    
-                  .bind('orientationchange', function(){
-                       if (window.orientation % 180 === 0){
-                           $(document.body).css("-webkit-transform-origin", "")
-                               .css("-webkit-transform", "");               
-                       } 
-                       else {                   
-                           if ( window.orientation > 0) { //clockwise
-                             $(document.body).css("-webkit-transform-origin", "200px 190px")
-                               .css("-webkit-transform",  "rotate(-90deg)");  
-                           }
-                           else {
-                             $(document.body).css("-webkit-transform-origin", "280px 190px")
-                               .css("-webkit-transform",  "rotate(90deg)"); 
-                           }
-                       }
-                   })
-                  .trigger('orientationchange'); 
-
+            //
+            function reorient(e) {
+                var portrait = (window.orientation % 180 == 0);
+                $("body > div").css("-webkit-transform", !portrait ? "rotate(-90deg)" : "");
+            }
+            window.onorientationchange = reorient;
+            window.setTimeout(reorient, 0);
         },
 
         // ===================================================================
